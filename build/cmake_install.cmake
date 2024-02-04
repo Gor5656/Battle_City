1017116886504e140cae6efa4a016c1e1e8cc398
Gor5656 <gorkocharyan.1113@gmail.com>
2024-02-04 14:37:53 +0400

    Added GLAD

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/gorko/Desktop/Battle_City/Battle_City/build/external/glfw/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/gorko/Desktop/Battle_City/Battle_City/build/external/glad/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
